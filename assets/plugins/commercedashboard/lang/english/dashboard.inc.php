c2f101176a0573efcc69641126db17a87fdf4c74
Marc <esens-2.5@ESENSs-iMac.fritz.box>
2019-02-04 12:15:14 +0100

    Add language

---
+++ assets/plugins/commercedashboard/lang/english/dashboard.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+setlocale(LC_ALL, 'en_US.UTF-8');
+
+return [
+    'dashboard.charts_title' => 'Order Schedule',
+    'dashboard.list_title' => 'Last 15 Orders',
+    'dashboard.date' => 'Date',
+    'dashboard.status' => 'Status',
+    'dashboard.amount' => 'Amount',
+    'dashboard.view' => 'View',
+    'dashboard.edit' => 'Edit',
+    'dashboard.thousands' => 'thou.',
+    'dashboard.amounts_current_month' => 'Amount in the current month',
+    'dashboard.amounts_prev_month' => 'Amount in the previous month',
+    'dashboard.count_current_month' => 'Orders in the current month',
+    'dashboard.count_prev_month' => 'Orders in the previous month',
+];
---
+++ assets/plugins/commercedashboard/lang/nederlands-utf8/dashboard.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+setlocale(LC_ALL, 'nl_NL.UTF-8');
+
+return [
+    'dashboard.charts_title' => 'Bestelschema',
+    'dashboard.list_title' => 'Laatste 15 bestellingen',
+    'dashboard.date' => 'Datum',
+    'dashboard.status' => 'Status',
+    'dashboard.amount' => 'Bedrag',
+    'dashboard.view' => 'Beeld',
+    'dashboard.edit' => 'Bewerken',
+    'dashboard.thousands' => 'duizend.',
+    'dashboard.amounts_current_month' => 'Bedrag in de huidige maand',
+    'dashboard.amounts_prev_month' => 'Bedrag in de voorgaande maand',
+    'dashboard.count_current_month' => 'Bestellingen in de huidige maand',
+    'dashboard.count_prev_month' => 'Bestellingen van de vorige maand',
+];
